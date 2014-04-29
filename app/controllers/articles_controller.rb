6156a07494bbb826e74a1a6b0185c6db9b919d87
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-04-29 22:29:14 +0530

    Imporve the Errors box, Forms, overall article look n feel

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :destroy]
 
 	def index
-		@articles = Article.all
+		@articles = Article.order(title: :desc)
 	end
 
 	def show
